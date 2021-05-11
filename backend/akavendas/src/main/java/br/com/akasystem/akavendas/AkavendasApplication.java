c94a8c38bb9b5086f2f2ef235467a5e6619d89f9
lucasrodrigues19 <pereirarodriguesl194@gmail.com>
2021-05-11 20:12:51 -0300

    Projeto criado

---
+++ backend/akavendas/src/main/java/br/com/akasystem/akavendas/AkavendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.akasystem.akavendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkavendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkavendasApplication.class, args);
+	}
+
+}
---
+++ backend/akavendas/src/test/java/br/com/akasystem/akavendas/AkavendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.akasystem.akavendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkavendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
