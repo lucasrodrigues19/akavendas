af75f22018279f95b3e65d4580beb92b1dde1033
lrodrigues19 <pereirarodriguesl194@gmail.com>
2021-07-03 07:23:37 -0300

    #ADDED integração barchart

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,6 +1,52 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SaleSuccess } from 'types/sale';
 import { ModuleKind } from 'typescript';
+import { round } from 'utils/formarter';
+import { BASE_URL } from 'utils/requests';
+type SeriesData = {
+    name: string;
+    data: number[];
+}
+type ChartData = {
+    labels:{
+        categories: string[];
+    };
+    series: SeriesData[]
+}
 const BarChart = () =>{
+    const [chartData,setChartData]= useState<ChartData>({
+        labels:{
+            categories: []
+        },
+        series:[
+            {
+                name: "",
+                data:[]
+            }
+        ]
+    })
+    useEffect(()=>{
+        axios.get(`${BASE_URL}/sales/success-by-seller`)
+        .then((_response) => {
+            const data = _response.data as SaleSuccess[]; 
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => round(100 * x.deals / x.visited,1))
+            setChartData({
+                labels:{
+                    categories: myLabels
+                },
+                series:[
+        
+                    {
+                        name: "%success",
+                        data:mySeries
+                    }
+                ]
+            });
+        })
+    },[])
     const options = {
         plotOptions:{
             bar: {
@@ -8,22 +54,22 @@ const BarChart = () =>{
             }
         },
     };
-    const mockData = {
-        labels:{
-            categories: ['Anakin','Barry Allen','Kal-El','Logan','Padmé']
-        },
-        series:[
+    // const mockData = {
+    //     labels:{
+    //         categories: ['Anakin','Barry Allen','Kal-El','Logan','Padmé']
+    //     },
+    //     series:[
 
-            {
-                name: "% Sucesso",
-                data:[43.3,67.1,70.1,80.1,90]
-            }
-        ]
-    }
+    //         {
+    //             name: "% Sucesso",
+    //             data:[43.3,67.1,70.1,80.1,90]
+    //         }
+    //     ]
+    // }
     return(
        <Chart
-            options={{...options,xaxis: mockData.labels,colors:["#7952b3"]}} //ela vai ser incorporada na definição do tsx, to dizendo aproveitar o options e vou passar mais argumentos para ele
-            series={mockData.series}
+            options={{...options,xaxis: chartData.labels,colors:["#7952b3"]}} //ela vai ser incorporada na definição do tsx, to dizendo aproveitar o options e vou passar mais argumentos para ele
+            series={chartData.series}
             type="bar"
             height="240" 
        
--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -11,28 +11,11 @@ type ChartData = {
 }
 
 const DonutChart = () =>{
-    //chartData variavel que vai ser o dado, setChartData: função para alterar o dado, ChartData: tipo generico do chartData, entre () o valor inicial
     const [chartData, setChartData] = useState<ChartData>({labels:[],series:[]})
-    //FORMA ERRADA, certo com o hocks, executando dessa forma, toda vez que o useState atualizar o estado, o componente vai verificar se tem algo para renderizar
-    // axios.get(`${BASE_URL}/sales/amount-by-seller`)
-    //     .then((_response) => {
-    //         const data = _response.data as SaleSum[]; //data vai ser do tipo SaleSum( o tipo que é retornado da requisição e foi criado em sale.ts)
-    //         const myLabels = data.map(x => x.sellerName);
-    //         const mySeries = data.map(x => x.sum)
-    //         setChartData({labels:myLabels, series: mySeries});
-    //         console.log(chartData)
-    //         console.log(_response.data)
-    //     })
-     //FORMA CORRETA, 1- primeiro parametro: a funçao que vai executar, 2 argumento: lista de objeto que o useEffect vai observar. com o useRequest, está ação so vai ser executada na
-    //instanciação do componente
-     // const mockData = {
-    //     series:[477138,799928,444867,220426,473088],
-    //     labels: ['Anakin','Barry Alen', 'Karl-el','Logan','Padmé']
-    // }
     useEffect(()=>{
         axios.get(`${BASE_URL}/sales/amount-by-seller`)
         .then((_response) => {
-            const data = _response.data as SaleSum[]; //data vai ser do tipo SaleSum( o tipo que é retornado da requisição e foi criado em sale.ts)
+            const data = _response.data as SaleSum[]; 
             const myLabels = data.map(x => x.sellerName);
             const mySeries = data.map(x => x.sum)
             setChartData({labels:myLabels, series: mySeries});
@@ -50,7 +33,7 @@ const DonutChart = () =>{
             type="donut"
             height="240" 
        
-       /> //retorna o componente do apexcharts que instalei, passo os dados como parametro
+       /> 
     );
 }
 export default DonutChart;
